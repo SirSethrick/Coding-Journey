2976bf0775b607c783871791fe3a0537984248b5
Seth Roll <93227874+SirSethrick@users.noreply.github.com>
2023-08-23 15:22:07 -0800

    Band Name.py
    This is my first project in my coding boot camp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #Go to: https://replit.com/@appbrewery/band-name-generator-start?v=1
 print("Welcome to the band name generator.")
+color = input("What is your favorite color?\n")
 city = input("Which city did you grow up in?\n")
 pet = input("What is the name of a pet?\n")
-print("Your band name could be " + city + " " + pet)
+print("Your band name could be " + color + " " + city + " " + pet)
